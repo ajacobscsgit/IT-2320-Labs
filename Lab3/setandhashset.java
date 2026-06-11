31ec7bbbf4009d236d52ac64803fc102e92dbd5c
anngel jacobs <ajacobscsgit@gmail.com>
2026-06-10 22:28:32 -0400

    Working on practice from the course.

---
+++ Lab3/setandhashset.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class setandhashset {
+    public static void main(String[] args) {
+        Set<String> names = new HashSet<>();
+
+        names.add("Anngel");
+        names.add("Erwashundah");
+        names.add("Asjimah");
+        names.add("Isha"); // Hash sets have no particular order
+       // Duplicate names show only once when ran
+        names.remove("Isha");
+        
+        System.out.println("These are the people I know:" + names);
+    }
+}
